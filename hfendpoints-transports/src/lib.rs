3de001f9aff0387651f327c58096dfe4893951a8
Morgan Funtowicz <funtowiczmo@gmail.com>
2025-04-01 16:54:45 +0200

    feat(openai): remove unused transport crate

--- hfendpoints-transports/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub enum TransportError {
-    ValidationError(String),
-}
-
-/// Utility type to represent a Result with associated TransportError type
-pub type TransportResult<T> = Result<T, TransportError>;
-
-pub trait Transport {}
